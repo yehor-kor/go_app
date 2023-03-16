c79ca2d1d5f403978ab0749d314844484baa3187
Yehor Korablin <yegor_ka2004@ukr.net>
2023-03-16 11:39:39 +0200

    Added lab2

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
+	start(100, 5)
+}
+
+func start(length, times int) {
 	startTime := time.Now()
 	quit := make(chan int)
-	length := 15
 	elem := rand.Intn(100)
 	var arr = []int {elem}
 	
@@ -22,22 +25,31 @@ func main() {
 	
 	var timeParts = []time.Time {}
 
-	for i := 0; i < 3; i++ {
+	for i := 0; i < times; i++ {
 		timeParts = append(timeParts, time.Now())
-		var part = arr[length/3 * i : length/3 * (i + 1)]
-		go calculate(part, i + 1, quit)
+		var startPos = length/times * i
+		var endPos int
+
+		if i < times - 1 {
+			endPos = length/times * (i + 1)
+		} else {
+			endPos = length/times * (i + 1) + length % times
+		}
+
+		var part = arr[startPos : endPos]
+		go calculate(part, i + 1, times, quit)
 	}
 	getTime(quit, timeParts, startTime)
 }
 
-func calculate(arr []int, index int, quit chan int) {
+func calculate(arr []int, index int, times int, quit chan int) {
 	for i := 0; i < len(arr); i++ {
 		arr[i] *= 2
 	}
 
-	fmt.Println(arr)
+	// fmt.Println(arr)
 
-	if index == 3 {
+	if index == times {
 		quit <- index
 	}
 }
